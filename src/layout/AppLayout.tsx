2d8ad7cb911977cc14ca0610f4f8c3dda0f33574
Suresh <sm@bicsom.co>
2025-11-02 10:46:14 +0530

    Footer fixed

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -3,32 +3,33 @@ import { Outlet } from "react-router";
 import AppHeader from "./AppHeader";
 import Backdrop from "./Backdrop";
 import AppSidebar from "./AppSidebar";
-import Footer from "../components/footer/Footer"; // ✅ import Footer
+import Footer from "../components/footer/Footer";
 
 const LayoutContent: React.FC = () => {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
 
   return (
-    <div className="min-h-screen xl:flex">
+    <div className="min-h-screen xl:flex flex-col lg:flex-row">
+      {/* ---------- Sidebar & Backdrop ---------- */}
       <div>
         <AppSidebar />
         <Backdrop />
       </div>
 
+      {/* ---------- Main Content ---------- */}
       <div
         className={`flex-1 flex flex-col transition-all duration-300 ease-in-out ${
           isExpanded || isHovered ? "lg:ml-[290px]" : "lg:ml-[90px]"
         } ${isMobileOpen ? "ml-0" : ""}`}
       >
-        {/* Header */}
         <AppHeader />
 
-        {/* Main Content */}
-        <main className="flex-1 p-4 mx-auto max-w-screen-2xl md:p-6">
+        {/* ---------- Page Content ---------- */}
+        <main className="flex-grow p-4 mx-auto max-w-screen-2xl md:p-6 w-full">
           <Outlet />
         </main>
 
-        {/* ✅ Footer */}
+        {/* ---------- Footer ---------- */}
         <Footer />
       </div>
     </div>
